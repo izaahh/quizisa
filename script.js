3555371bbd4d7503d2ae2abf78b9fc4e825637d2
izaahh <131893386+izaahh@users.noreply.github.com>
2024-08-06 12:14:42 -0300

    OI

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 const perguntas = [
     {
-        enunciado: "PERGUNTA 1",
+        enunciado: "QUAL É A PRINCIPAL FUNÇÃO DE UM MÉDICO",
         alternativas: [
             {
-                texto: "RESPOSTA 1!",
-                afirmacao: "AFIRMATIVA DA RESPOSTA 1r. "
+                texto: "Diagnosticar e tratar doenças",
+                afirmacao: "Os medicos sao responsaveis pela saude dos pacientes,realizando diagnosticos e tratamentos."
             },
             {
-                texto: "RESPOSTA 2",
-                afirmacao: "AFIRMATIVA RESPOSTA 2."
+                texto: "Ensinar matéria escolares ",
+                afirmacao: "A principal funcao de um medico e diagnosticar e tratar doencas,nao ensinar."
             }
         ]
     },
     {
-        enunciado: "PERGUNTA 2",
+        enunciado: "O QUE FAZ UM ENGENHEIRO CIVIL",
         alternativas: [
             {
-                texto: "RESPOSTA 1",
-                afirmacao: "AFIRMATIVA RESPOSTA 1"
+                texto: "PROJECTA E CONSTROI EDIFICIOS",
+                afirmacao: "muito bem! engenheiros civis trabalham na concepçao,como pontes, estradas e edificios"
             },
             {
-                texto: "RESPOSTA 2",
-                afirmacao: "AFIRMATIVA RESPOSTA 2"
+                texto: "CONSERTA ELETRODOMESTICOS",
+                afirmacao: "Engenheiro civis projetam e constroem edificios e infraestruturas,eles nao concertam eletrodomesticos."
             }
         ]
     },
     {
-        enunciado: "PERGUNTA 3",
+        enunciado: "QUAL A PRINCIPAL FUNÇÃO DE UM PROFESSOR",
         alternativas: [
             {
-                texto: "RESPOSTA 1",
+                texto: "GERIR UMA EMPRESA",
                 afirmacao: "AFIRMATIVA RESPOSTA 1"
             },
             {
-                texto: "RESPOSTA 2",
+                texto: "ENSINAR E EDUCAR ALUNOS",
                 afirmacao: "AFIRMATIVA RESPOSTA 2."
             }
         ]
