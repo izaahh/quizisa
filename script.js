7701d542d23a77756bc6bf1fe961d5bb3bbb61e7
izaahh <131893386+izaahh@users.noreply.github.com>
2024-08-06 11:51:59 -0300

    oi

---
+++ script.js
@@ -0,0 +1,42 @@
+const perguntas = [
+    {
+        enunciado: "PERGUNTA 1",
+        alternativas: [
+            {
+                texto: "RESPOSTA 1!",
+                afirmacao: "AFIRMATIVA DA RESPOSTA 1r. "
+            },
+            {
+                texto: "RESPOSTA 2",
+                afirmacao: "AFIRMATIVA RESPOSTA 2."
+            }
+        ]
+    },
+    {
+        enunciado: "PERGUNTA 2",
+        alternativas: [
+            {
+                texto: "RESPOSTA 1",
+                afirmacao: "AFIRMATIVA RESPOSTA 1"
+            },
+            {
+                texto: "RESPOSTA 2",
+                afirmacao: "AFIRMATIVA RESPOSTA 2"
+            }
+        ]
+    },
+    {
+        enunciado: "PERGUNTA 3",
+        alternativas: [
+            {
+                texto: "RESPOSTA 1",
+                afirmacao: "AFIRMATIVA RESPOSTA 1"
+            },
+            {
+                texto: "RESPOSTA 2",
+                afirmacao: "AFIRMATIVA RESPOSTA 2."
+            }
+        ]
+    },
+      
+];
